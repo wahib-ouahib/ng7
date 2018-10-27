c42d04356bfac2f183a60f6b055a7c058a20fc34
wahib <wahib.med.ouahib@gmail.com>
2018-10-27 10:27:24 +0100

    :art: connecting to posts API

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { observable } from 'rxjs';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  users$: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(
+      data => this.users$ = data
+    )
+  }
+
+}
